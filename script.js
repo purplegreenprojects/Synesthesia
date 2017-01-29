f303d74ab36acbdb760d7e0bb61020d33704cd14
eef22 <eford1@binghamton.edu>
2017-01-28 19:21:58 -0500

    getting closer; counter

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
 
 	//make dynamic
-	var sentence = ("abcdcdcdaabbccdd");
+	var sentence = ("abcdcdcdabc");
 	var eachLetter = sentence.split("");
-;
-	//eachLetter = ["a", "b", "c", "d"]
-		// console.log(eachLetter);
-		// console.log(eachLetter[2]);
 
-	
+	//eachLetter = ["a", "b", "c", "d"]
+		console.log(eachLetter);
+		console.log(eachLetter[2]);
+
 	var i = 0;
-	var letterX = i++;
+	var letterNumber = i++;
 
 	$("#text").empty()
-		// .append("<div class='a'>" + eachLetter[0] + "</div>")
-		// .append("<div class='" + eachLetter[0] + "'>" + eachLetter[0] + "</div>")
-		.append("<div class='" + eachLetter[0] + "'>" + eachLetter[0] + "</div>")
-		.append("<div class='" + eachLetter[1] + "'>" + eachLetter[1] + "</div>")
-		.append("<div class='" + eachLetter[2] + "'>" + eachLetter[2] + "</div>")
-		.append("<div class='" + eachLetter[3] + "'>" + eachLetter[3] + "</div>");
+		// .append(eachLetter[0])
+		// .append(eachLetter[1])
+		// .append(eachLetter[2])
+		// .append(eachLetter[3])
+		// .append(eachLetter[4])
+		// .append(eachLetter[5])
+		// .append(eachLetter[6])
+		// .append(eachLetter[7])
+		// .append(eachLetter[8])
+		// .append(eachLetter[9])
+		// .append(eachLetter[10]);
 
-	// console.log(sentence[letterX]);
-	// console.log(sentence[letterX]);
-	// console.log(sentence[letterX]);
-	// console.log(sentence[letterX]);
-
-	// $("#text").empty().append(sentence);
-
+		.append(eachLetter[letterNumber])
+		.append(eachLetter[letterNumber+1]);
 
 });
+
+
+// $("#text").empty()
+	// 	.append("<div class='" + eachLetter[0] + "'>" + eachLetter[0] + "</div>")
+	// 	.append("<div class='" + eachLetter[1] + "'>" + eachLetter[1] + "</div>")
+	// 	.append("<div class='" + eachLetter[2] + "'>" + eachLetter[2] + "</div>")
+	// 	.append("<div class='" + eachLetter[3] + "'>" + eachLetter[3] + "</div>");
+
+
+
+// $(sentence).each(function(i))
