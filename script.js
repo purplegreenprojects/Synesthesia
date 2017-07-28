720aaf74131fa592e13c8b9b64fa32426ec877f0
eef22 <eef22@users.noreply.github.com>
2017-07-28 18:50:45 -0400

    add Swoosh effect to hoverText

--- script.js
+++ script.js
@@ -19,6 +19,33 @@ $(document).ready(function() {
 //call the function
 	$("#originalText").on("keyup", function() {
 		printText()
+		clearInterval(window.finishTyping)
+
+		window.finishTyping = setTimeout(function(){
+			swoosh()
+		},3000)
 	})
+
+//swoosh function
+	function swoosh(){
+		console.log("swooshing")
+		window.swooshLoop = setInterval(function(){
+			var swooshing = $(".swoosh")
+			if (swooshing.length == 0) {
+				$("#hoverText").find("span").first().addClass("swoosh")
+			}
+			else {
+				$(".swoosh").removeClass("swoosh")
+
+				var nextSwoosh = $(swooshing).next()
+				if (nextSwoosh.length !== 0) {
+					$(nextSwoosh).addClass("swoosh")
+				}
+				else {
+					clearInterval(window.swooshLoop)
+				}
+			}
+		},100)
+	}
 
 });
